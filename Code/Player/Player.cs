d3f8005802148ef783f700e70b087190b2bd430f
Diego Esedin <diego.e8@hotmail.com>
2025-09-07 01:30:48 -0300

    Add placeholders and basic first player movement

--- Code/Player/Player.cs
+++ Code/Player/Player.cs
@@ -1,10 +1,27 @@
 using Godot;
 using System;
 
-public partial class Player : Sprite2D
+public partial class Player : CharacterBody2D
 {
+	public override void _Ready()
+	{
+		
+	}
+
 	public override void _Process(double delta)
 	{
-		base._Process(delta);
+		Vector2 vel = new Vector2(0, 0);
+		if (Input.IsActionPressed("ui_right"))
+		{
+			vel.X += 50;
+		}
+		else if (Input.IsActionPressed("ui_left"))
+		{
+			vel.X -= 50;
+		}
+		vel.Y += 400 * (float)delta;
+
+		Velocity = vel;
+		MoveAndSlide();
 	}
 }
