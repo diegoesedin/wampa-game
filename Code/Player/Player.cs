56372a194b289561b4f9406571946f29781e2694
Diego Esedin <diego.e8@hotmail.com>
2025-09-13 16:44:17 -0300

    Update movement

--- Code/Player/Player.cs
+++ Code/Player/Player.cs
@@ -3,6 +3,12 @@ using System;
 
 public partial class Player : CharacterBody2D
 {
+	private const int SPEED = 2000;
+	private const int GRAVITY = 2000;
+	private const float FRICTION = .01f;
+	private const float ACCELERATION = 1.5f;
+	private const float JUMP_FORCE = 500;
+	
 	public override void _Ready()
 	{
 		
@@ -11,20 +17,22 @@ public partial class Player : CharacterBody2D
 	public override void _Process(double delta)
 	{
 		Vector2 vel = new Vector2(0, 0);
+		int direction = 0;
 		if (Input.IsActionPressed("ui_right"))
-		{
-			vel.X += 50;
-		}
+			direction += 1;
 		else if (Input.IsActionPressed("ui_left"))
-		{
-			vel.X -= 50;
-		}
+			direction -= 1;
+		
+		if (direction != 0)
+			vel.X = Mathf.Lerp(vel.X, direction * SPEED, ACCELERATION * (float)delta);
+		else
+			vel.X = Mathf.Lerp(vel.X, 0, FRICTION * (float)delta);
 
 		if (Input.IsActionJustPressed("jump") && IsOnFloor())
 		{
-			vel.Y -= 2000;
+			vel.Y -= JUMP_FORCE;
 		}
-		vel.Y += 400 * (float)delta;
+		vel.Y += GRAVITY * (float)delta;
 
 		Velocity = vel;
 		MoveAndSlide();
